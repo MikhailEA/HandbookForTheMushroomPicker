fa5f8d12053b76f6630d2081b37c47a8b1a648c8
MikhailEA <michev.work@gmail.com>
2021-10-20 18:44:21 +0300

    Изменение заголовка списка

--- app/src/main/java/com/android/handbookforthemushroompicker/MainActivity.java
+++ app/src/main/java/com/android/handbookforthemushroompicker/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.Toast;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -27,19 +26,22 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ListView list;
     private String[] array;
     private ArrayAdapter<String> adapter;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         list = findViewById(R.id.listView);
         array = getResources().getStringArray(R.array.mushrooms_array);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
                 new ArrayList<String>(Arrays.asList(array)));
         list.setAdapter(adapter);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -54,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        toolbar.setTitle(R.string.mushrooms);
         return true;
     }
 
@@ -62,6 +65,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = menuItem.getItemId();
         if (id == R.id.id_mushrooms)
         {
+            toolbar.setTitle(R.string.mushrooms);
+
             array = getResources().getStringArray(R.array.mushrooms_array);
             adapter.clear();
             adapter.addAll(array);
@@ -71,12 +76,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.id_edible_mushrooms) {
 
+            toolbar.setTitle(R.string.edible_mushrooms);
+
             array = getResources().getStringArray(R.array.edible_mushrooms_array);
             adapter.clear();
             adapter.addAll(array);
             adapter.notifyDataSetChanged();
 
         } else if (id == R.id.id_poisonous_mushrooms) {
+
+            toolbar.setTitle(R.string.poisonous_mushrooms);
 
             array = getResources().getStringArray(R.array.poisonous_mushrooms_array);
             adapter.clear();
@@ -85,6 +94,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.id_mushrooms_are_edible) {
 
+            toolbar.setTitle(R.string.mushrooms_are_edible);
+
             array = getResources().getStringArray(R.array.mushrooms_are_edible_array);
             adapter.clear();
             adapter.addAll(array);
@@ -92,12 +103,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.id_mushroom_stories) {
 
+            toolbar.setTitle(R.string.mushroom_stories);
+
             array = getResources().getStringArray(R.array.mushroom_stories_array);
             adapter.clear();
             adapter.addAll(array);
             adapter.notifyDataSetChanged();
 
         } else if (id == R.id.id_video) {
+
+            toolbar.setTitle(R.string.video);
 
             array = getResources().getStringArray(R.array.video_array);
             adapter.clear();
