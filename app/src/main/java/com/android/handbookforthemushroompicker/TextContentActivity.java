74087002b2d33cf60ecfc45fad9fcda13cc63129
MikhailEA <michev.work@gmail.com>
2021-11-09 18:46:30 +0300

    редактирование текста и картинок

--- app/src/main/java/com/android/handbookforthemushroompicker/TextContentActivity.java
+++ app/src/main/java/com/android/handbookforthemushroompicker/TextContentActivity.java
@@ -1,15 +1,65 @@
 package com.android.handbookforthemushroompicker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TextContentActivity extends AppCompatActivity {
+    private TextView text_content;
+    private ImageView iContent;
+    private int category = 0;
+    private int position = 0;
+    private int [] mushrooms_array ={R.string.mushrooms1, R.string.mushrooms2,
+            R.string.mushrooms3, R.string.mushrooms4, R.string.mushrooms5,
+            R.string.mushrooms6, R.string.mushrooms7, R.string.mushrooms8,
+            R.string.mushrooms9, R.string.mushrooms10, R.string.mushrooms11};
+    private int [] edible_mushrooms_array ={R.string.edible_mushrooms1, R.string.edible_mushrooms2,
+            R.string.edible_mushrooms3, R.string.edible_mushrooms4, R.string.edible_mushrooms5,
+            R.string.edible_mushrooms, R.string.edible_mushrooms7, R.string.edible_mushrooms8,};
+    private int [] image_mushrooms ={R.drawable.boletus, R.string.edible_mushrooms2,};
+
+
+
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.text_content);
+        text_content = findViewById(R.id.text_main_content);
+        iContent = findViewById(R.id.imageContent);
+        reciveIntent();
+    }
+    private void reciveIntent() {
+        Intent i = getIntent();
+        if (i != null) {
+            category = i.getIntExtra("category", 0);
+            position = i.getIntExtra("position", 0);
+        }
+        switch (category) {
+            case 0:
+                iContent
+                text_content.setText(mushrooms_array[position]);
+                break;
+            case 1:
+                text_content.setText(edible_mushrooms_array[position]);
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+
+        }
     }
 }
