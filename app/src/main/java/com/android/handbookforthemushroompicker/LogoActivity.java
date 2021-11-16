40370d84d5b972a7b337ca32ffa14632ad7b2095
MikhailEA <michev.work@gmail.com>
2021-11-16 17:10:20 +0300

    Добавление лого Активити и его инициализация

--- app/src/main/java/com/android/handbookforthemushroompicker/LogoActivity.java
+++ app/src/main/java/com/android/handbookforthemushroompicker/LogoActivity.java
@@ -1,7 +1,9 @@
 package com.android.handbookforthemushroompicker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 
@@ -9,6 +11,19 @@ public class LogoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo_activity);
 
+    }
+
+    public void onClickStart(View view)
+    {
+        Intent i = new Intent(LogoActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
     }
 }
