f604cc50160caa76fc7686f7f52ae60543860be2
MikhailEA <michev.work@gmail.com>
2021-11-17 18:39:58 +0300

    Добавление анимации логотипа

--- app/src/main/java/com/android/handbookforthemushroompicker/LogoActivity.java
+++ app/src/main/java/com/android/handbookforthemushroompicker/LogoActivity.java
@@ -4,15 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 public class LogoActivity extends Activity {
+    private Animation logoAnim, buttonLogoAnim;
+    private Button bAnim;
+
+    private ImageView logoImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logo_activity);
+        init();
 
+    }
+
+    private void init() {
+        logoAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.logo_anim);
+        logoImage = findViewById(R.id.logoView);
+        bAnim = findViewById(R.id.buttonAnim);
     }
 
     public void onClickStart(View view)
