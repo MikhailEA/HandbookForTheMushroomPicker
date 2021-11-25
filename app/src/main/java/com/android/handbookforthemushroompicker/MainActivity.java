cebe325bbd32a39e9d695183f045002f706facaa
MikhailEA <michev.work@gmail.com>
2021-11-25 18:38:10 +0300

    добавление кнопки драйвер меню

--- app/src/main/java/com/android/handbookforthemushroompicker/MainActivity.java
+++ app/src/main/java/com/android/handbookforthemushroompicker/MainActivity.java
@@ -51,6 +51,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
 
 
 
